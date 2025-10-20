bc74a6e674093a766b745760d54ece07d0409449
Ash <anaswar.ash@outlook.com>
2025-10-20 23:00:27 +0100

    first

---
+++ api/errors.py
@@ -0,0 +1,11 @@
+from flask import jsonify
+
+def bad_request(message):
+    response = jsonify({'error': 'bad request', 'message': message})
+    response.status_code = 400
+    return response
+
+def internal_error(message):
+    response = jsonify({'error': 'internal server error', 'message': message})
+    response.status_code = 500
+    return response
